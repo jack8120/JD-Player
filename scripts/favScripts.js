6ecaae8b5165bb252c5d098cf8438d83133c939e
Jack Torn <j4cktorn@gmail.com>
2022-09-21 20:06:28 +0100

    mvp favourites / landing

--- scripts/favScripts.js
+++ scripts/favScripts.js
@@ -5,12 +5,19 @@ console.log('Connected?');
 //Global variables
 let favSong;
 let favArtist;
+let favGenre;
+// let favDOB;
 // let favGenre;
 // let dob;
 
 //Elements
-let favouriteSongElement = document.getElementById('favSong');
-let favouriteArtistElement = document.getElementById('favArtist');
+let favouriteSongElement   = document.getElementById('faveSong');
+let favouriteArtistElement = document.getElementById('faveArtist');
+let favouriteGenreElement  = document.getElementById('faveGenre');
+// let favDOBElement          = document.getElementById('faveDOB');
+
+
+
 
 let jackFave;
 
@@ -20,6 +27,8 @@ class favourites{
     {
         this.favSong    = favSong;
         this.favArtist  = favArtist;
+        this.favGenre   = favGenre;
+        // this.favDOB     = favDOB;
     }
    
 }
@@ -32,17 +41,27 @@ function formSubmitted(event){
 
     favSong = event.target.favSong.value;
     favArtist = event.target.favArtist.value;
+    favGenre = event.target.favGenre.value;
+    // favDOB = event.target.favDOB.value;
 
 // pass value to new instance jackFav
 
     jackFave = new favourites(favSong, favArtist);
 
-    favouriteSongElement.innerHTML = jackFave.favSong;
+    favouriteSongElement.innerHTML   = jackFave.favSong;
     favouriteArtistElement.innerHTML = jackFave.favArtist;
-     console.log(jackFave.favSong);
-     console.log(jackFave.favArtist);
-     document.getElementById('faveSong').innerHTML  = "Your favourite song is: " + jackFave.favSong;
+    favouriteGenreElement.innerHTML  = jackFave.favGenre;
+    // favDOBElement.innerHTML          = jackFave.favDOB;
+
+    //  console.log(jackFave.favSong);
+    //  console.log(jackFave.favArtist);
+     document.getElementById('faveSong').innerHTML    = "Your favourite song is: "   + jackFave.favSong;
      document.getElementById('faveArtist').innerHTML  = "Your favourite Artist is: " + jackFave.favArtist;
+     document.getElementById('faveGenre').innerHTML   = "Your favourite Genre is: "  + jackFave.favGenre;
+    //  
+    document.getElementById("myForm").reset();
 }
+
+
 
   
