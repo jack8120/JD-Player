d066da5a244a787722b188ddad5d131a9cc8f9a6
Jack Torn <j4cktorn@gmail.com>
2022-09-26 12:01:19 +0100

    styling landing page and adding sections to favourites form

--- scripts/favScripts.js
+++ scripts/favScripts.js
@@ -6,18 +6,21 @@ console.log('Connected?');
 
 
 //Global variables
+
+let favName;
 let favSong;
 let favArtist;
 let favGenre;
-// let favDOB;
-// let favGenre;
-// let dob;
+let favDob;
+
+
 
 //Elements
+let favouriteNameElement   = document.getElementById('faveName');
 let favouriteSongElement   = document.getElementById('faveSong');
 let favouriteArtistElement = document.getElementById('faveArtist');
 let favouriteGenreElement  = document.getElementById('faveGenre');
-// let favDOBElement          = document.getElementById('faveDOB');
+let favouriteDobElement    = document.getElementById('faveDOB');
 
 
 
@@ -26,12 +29,13 @@ let jackFave;
 
 class favourites{
 
-    constructor(favSong, favArtist)
+    constructor(favName, favSong, favArtist, favGenre, favDob)
     {
+        this.favName    = favName;
         this.favSong    = favSong;
         this.favArtist  = favArtist;
         this.favGenre   = favGenre;
-        // this.favDOB     = favDOB;
+        this.favDob     = favDob;
     }
    
 }
@@ -42,27 +46,41 @@ function formSubmitted(event){
 
     event.preventDefault();
 
-    favSong = event.target.favSong.value;
+    favName   = event.target.favName.value;
+    favSong   = event.target.favSong.value;
     favArtist = event.target.favArtist.value;
-    favGenre = event.target.favGenre.value;
+    favGenre  = event.target.favGenre.value;
+    favDob    = event.target.favDob.value;
     // favDOB = event.target.favDOB.value;
 
 // pass value to new instance jackFav
 
-    jackFave = new favourites(favSong, favArtist);
+    jackFave = new favourites(favName, favSong, favArtist, favGenre, favDob);
 
+    favouriteNameElement.innerHTML   = jackFave.favName;
     favouriteSongElement.innerHTML   = jackFave.favSong;
     favouriteArtistElement.innerHTML = jackFave.favArtist;
     favouriteGenreElement.innerHTML  = jackFave.favGenre;
-    // favDOBElement.innerHTML          = jackFave.favDOB;
+    favouriteDobElement.innerHTML    = jackFave.favDob;
+
+    function reverseString(str) {
+        var splitString = str.split("");
+        var reverseArrray = splitString.reverse();
+        var joinArray = reverseArrray.join("");
+        return joinArray;
+    }
+
+    
 
     //  console.log(jackFave.favSong);
     //  console.log(jackFave.favArtist);
-     document.getElementById('faveSong').innerHTML    = "Your favourite song is: "   + jackFave.favSong;
-     document.getElementById('faveArtist').innerHTML  = "Your favourite Artist is: " + jackFave.favArtist;
-     document.getElementById('faveGenre').innerHTML   = "Your favourite Genre is: "  + jackFave.favGenre;
+    document.getElementById('faveNames').innerHTML    = "Thankyou " + jackFave.favName + ". Your favourites are: ";
+    document.getElementById('faveSongs').innerHTML    = "Your favourite song is: "   + jackFave.favSong;
+    document.getElementById('faveArtists').innerHTML  = "Your favourite Artist is: " + jackFave.favArtist;
+    document.getElementById('faveGenres').innerHTML   = "Your favourite Genre is: "  + jackFave.favGenre;
+    document.getElementById('faveDOBs').innerHTML     = "Your date of birth is: "    + jackFave.favDob;
     //  
-    document.getElementById("myForm").reset();
+    // document.getElementById("myForm").reset();
 }
 
 
